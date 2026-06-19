526f75fdc4f5d530f1225362dfffbc95bda02c13
srjonjoy <ghoshsrinjoy2@gmail.com>
2026-06-19 22:43:19 +0530

    added pivot

---
+++ array/Pivot.js
@@ -0,0 +1,14 @@
+const nums=[1,7,3,6,5,6]
+let leftsum=0
+let rightSum=nums.reduce((sum,a)=>sum+a,0)
+
+for(let i=0;i<nums.length;i++){
+    rightSum=rightSum-nums[i]
+    if(leftsum===rightSum){
+      console.log(i)
+      break
+         
+    }
+    leftsum=leftsum+nums[i]
+
+}
