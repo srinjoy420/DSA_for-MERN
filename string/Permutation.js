0d1432db01cd9a1189e6d6350044a4a37cf676e7
srjonjoy <ghoshsrinjoy2@gmail.com>
2026-07-21 12:59:32 +0530

    added

---
+++ string/Permutation.js
@@ -0,0 +1,6 @@
+function hasAB(s) {
+    return s.includes('ab');
+}
+
+console.log(hasAB("acccb")); // true
+console.log(hasAB("abc"));      // false
--- string/longest_subsequence.js
+++ string/longest_subsequence.js
@@ -1,11 +1,19 @@
-const input="aaa"
-let char={}
-let str=[]
-for(let key of input){
-    char[key]=(char[key] || 0)+1
-}
-for(let i in char){
-  
-}
-console.log(str);
+function longestSubSequence(s){
+    let totalStringLength=0
+    for(let i=0;i<s.length;i++){
+        let currentStringSet=new Set()
+        for(let j=i;j<s.length;j++){
+            if(currentStringSet.has(s[j])){
+                break
+            }
+            else{
+                currentStringSet.add(s[j])
+            }
+        }
+         totalStringLength=Math.max(totalStringLength,currentStringSet.size)
 
+    }
+    return totalStringLength
+}
+
+console.log(longestSubSequence("abcdefabcbb"));
