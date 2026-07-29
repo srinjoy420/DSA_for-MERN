9e1c672071a19b5a37578357028647b5e98b604e
srjonjoy <ghoshsrinjoy2@gmail.com>
2026-07-29 19:24:31 +0530

    added the update

--- array/validamalgum.js
+++ array/validamalgum.js
@@ -1,15 +1,11 @@
-function ValidAmulgum(s1,s2){
-    if(s1.length!==s2.length) return false
-    s1=s1.split("").sort().join("")
-    s2=s2.split("").sort().join("")
-    return s1===s2
-
+function isAnagram(s1, s2) {
+    if (s1.length !== s2.length) return false;
+    s1 = s1.split("").sort().join("");
+    s2 = s2.split("").sort().join("");
+    return s1 === s2;
 }
 
-const s1="listen"
-const s2="silent"
-if(ValidAmulgum(s1,s2)){
-    return true
-}else{
-    return false
-}
+const s1 = "listen";
+const s2 = "silent";
+
+console.log(isAnagram(s1, s2)); // true
