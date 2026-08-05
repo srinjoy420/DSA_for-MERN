7860a8121ee13c7931a587c94abd6e7860baa28b
srjonjoy <ghoshsrinjoy2@gmail.com>
2026-08-05 19:44:23 +0530

    added the result

--- array/TwoSum.js
+++ array/TwoSum.js
@@ -16,7 +16,9 @@ function twoSum(nums,target){
     for(let i=0;i<nums.length;i++){
         for(let j=0;j<nums.length;j++){
             if(nums[i]+nums[j]===target){
-                return [i,j]
+                 return [i,j]
+                //if i have to display the valus not the index
+                // return [nums[i],nums[j]]
             }
         }
     }
