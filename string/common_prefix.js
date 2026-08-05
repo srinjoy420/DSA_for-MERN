050d5365a039cb62193e6fdac5ad69d57a1cf218
srjonjoy <ghoshsrinjoy2@gmail.com>
2026-08-05 21:16:51 +0530

    added

--- string/common_prefix.js
+++ string/common_prefix.js
@@ -2,6 +2,8 @@ const arr=["flower","flow","flight"]
 let first=arr[0]
 let last=arr[arr.length-1]
 let maxstring=Math.max(first.length,last.length)
+
+
 console.log(maxstring);
 let i=0
 while( i <maxstring && first[i]===last[i]){
